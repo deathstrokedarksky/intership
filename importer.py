390d9b1de5485b9d530d9284b6d938e1c6b27fd5
Yerkebulan <42295524+deathstrokedarksky@users.noreply.github.com>
2022-06-17 15:42:40 +0600

    Base code and files

---
+++ importer.py
@@ -0,0 +1,10 @@
+import os
+#all 5 main files for the work
+
+os.path.join((os.path.dirname(__file__)), 'Исходные данные', 'Пресс' )
+
+#CAMAC_data = open()
+#LCR_data = open()
+#Flukr_data = open()
+#LAI24_data = open()
+#Press_data = open()
